01f8d1e55ec830b357b8d9b9690597c4f5246c69
rsstdd <rosstod@gmail.com>
2016-09-19 13:34:27 -0700

    add the read subcommand

--- guests.js
+++ guests.js
@@ -4,12 +4,21 @@ const fs = require('fs');
 const path = require('path');
 const guestsPath = path.join(__dirname, 'guests.json'); //__dirname is the name of the dir that you are in
 
-fs.readFile(guestsPath, 'utf8', (err, data) => {
-  if (err) {
-    throw err;
-  }
+const node = path.basename(process.argv[0]); //full path to node
+const title = path.basename(process.argv[1]); //give you the file. perhaps the full path to that file
+const cmd = process.argv[2]; // any additional arguments
 
-  const guests = JSON.parse(data);
+if (cmd === 'read') {
+  fs.readFile(guestsPath, 'utf8', (err, data) => {
+    if (err) {
+      throw err;
+    }
 
-  console.log(guests);
-})
+    const guests = JSON.parse(data);
+
+    console.log(guests);
+  });
+} else {
+  console.error(`Usage: ${node} ${title} read`);
+  process.exit(1); //exit strategy - anything non-zero
+}
