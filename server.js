2b42774531273e6b3c8bc867340775015c4d788c
rsstdd <rosstod@gmail.com>
2016-09-20 13:15:37 -0700

    Send all guest records form the database

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
+const guestsPath = path.join(__dirname, 'guests.json');
+
 const http = require('http');
 const port = process.env.PORT || 8000;
 
 const server = http.createServer((req, res) => {
   if (req.method === 'GET' && req.url === '/guests') {
-    const guests = ['Mary', 'Don'];
-    const guestsJSON = JSON.stringify(guests);
+    fs.readFile(guestsPath, 'utf8', (err, guestsJSON) => {
+      if (err) {
+        console.error(err.stack);
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        return res.end('Internal Server Error');
+      }
 
-    res.setHeader('Content-Type', 'application/json');
-    res.end(guestsJSON);
+      res.setHeader('Content-Type', 'application/json');
+      res.end(guestsJSON);
+    });
   } else {
     res.statusCode = 404;
     res.setHeader('Content-Type', 'text/plain');
@@ -19,4 +29,4 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, () => {
   console.log(`Listening on port: ${port}`);
-})
+});
