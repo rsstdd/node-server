6936682c0d0510b564b3a2dccc6eadbe29892b28
rsstdd <rosstod@gmail.com>
2016-09-20 13:37:33 -0700

    Send individual guest records from the database

--- server.js
+++ server.js
@@ -20,6 +20,36 @@ const server = http.createServer((req, res) => {
       res.setHeader('Content-Type', 'application/json');
       res.end(guestsJSON);
     });
+  } else if (req.method === 'GET' && req.url === '/guests/0') {
+    fs.readFile(guestsPath, 'utf8', (err, guestsJSON) => {
+      if (err) {
+        console.error(err.stack);
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        return res.end('Internal Server Error');
+      }
+
+      const guests = JSON.parse(guestsJSON);
+      const guestJSON = JSON.stringify(guests[1]);
+
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(guestJSON);
+    });
+  } else if (req.method === 'GET' && req.url === '/guests/1') {
+    fs.readFile(guestsPath, 'utf8', (err, guestsJSON) => {
+      if (err) {
+        console.error(err.stack);
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        return res.end('Internal Server Error');
+      }
+
+      const guests = JSON.parse(guestsJSON);
+      const guestJSON = JSON.stringify(guests[1]);
+
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(guestJSON);
+    });
   } else {
     res.statusCode = 404;
     res.setHeader('Content-Type', 'text/plain');
