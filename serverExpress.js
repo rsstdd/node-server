2148afb2302d273c8d8eb0b7a6fec59586294d91
rsstdd <rosstod@gmail.com>
2016-09-21 13:19:51 -0700

    Send individual guest records from the database

--- serverExpress.js
+++ serverExpress.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
+const guestsPath = path.join(__dirname, 'guests.json');
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000;
@@ -7,9 +11,36 @@ const port = process.env.PORT || 8000;
 app.disable('x-powered-by');
 
 app.get('/guests', (req, res) => {
-  const guests = ['something', 'yep', 'now', 'yes'];
-  res.send(guests);
-}); 
+  fs.readFile(guestsPath, 'utf8', (err, guestsJSON) => {
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+
+    const guests = JSON.parse(guestsJSON);
+
+    res.send(guests);
+  })
+});
+
+app.get('/guests/:id', (req, res) => {
+  fs.readFile(guestsPath, 'utf8', (err, guestsJSON) => {
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+
+    const id = Number.parseInt(req.params.id);
+    const guests = JSON.parse(guestsJSON);
+
+    if (id < 0 || id >= guests.length || Number.isNaN(id)) {
+      return res.sendStatus(404);
+    }
+
+    res.set('Content-Type', 'text/plain');
+    res.send(guests[id]);
+  });
+});
 
 app.use((req, res) => {
   res.sendStatus(404);
